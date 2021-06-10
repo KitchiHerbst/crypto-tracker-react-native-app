61461b3525ba0000ccfcf0d885862ec8e43aa052
Triston <tristonherbst97@gmail.com>
2021-06-09 21:20:48 -0500

    beginning of the welcome screen

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my first React Native app to help me keep track of certain crypto-currencies prices.
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <Text style={styles.name}>Crypto-Tracker</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "dodgerblue",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 300,
+    height: 100,
+    backgroundColor: "tomato",
+    marginBottom: 200,
+    borderRadius: 20,
+    // alignContent: 'center'
+  },
+  name: {
+    fontSize: 45,
+    textAlign: "center",
+    paddingTop: 20,
+  },
+});
+
+export default WelcomeScreen;
